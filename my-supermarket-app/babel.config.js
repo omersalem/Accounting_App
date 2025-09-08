f530f8437471c6bb0c6b0143d7d077a67a5794b5
omersalem <omersalem2008@hotmail.com>
2025-09-08 13:34:44 +0300

    build(android): configure package/com.omersalem.accountingapp, add eas.json (APK preview), Expo extra Firebase keys

--- my-supermarket-app/babel.config.js
+++ my-supermarket-app/babel.config.js
@@ -1,8 +1,9 @@
 module.exports = function (api) {
+  api.cache(true);
   const isTest = api.env('test');
   return {
     presets: ['babel-preset-expo'],
-    // Disable NativeWind plugin in Jest to avoid transform issues
+    // Enable NativeWind in app builds; disable in tests for Jest stability.
     plugins: isTest ? [] : ['nativewind/babel'],
   };
 };
